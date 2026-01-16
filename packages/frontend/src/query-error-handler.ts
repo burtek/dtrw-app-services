02ef9a1b9170214f731f9ba73f3440983bfcef23
Bartosz <burtekdotryw@gmail.com>
2026-01-17 00:06:07 +0100

    fix(ui): error handling

--- packages/frontend/src/query-error-handler.ts
+++ packages/frontend/src/query-error-handler.ts
@@ -6,6 +6,8 @@ function handleStatusCode(status: number, details?: string): string {
     const withDetails = (error: string) => (details ? `${error}: ${details}` : error);
 
     switch (status) {
+        case 400:
+            return withDetails('Bad Request');
         case 401:
             return withDetails('Unauthorized');
         case 403:
@@ -14,27 +16,44 @@ function handleStatusCode(status: number, details?: string): string {
             return withDetails('Not Found');
         case 500:
             return withDetails('Internal Server Error');
+        case 502:
+            return withDetails('Bad Gateway - retry later');
+        case 503:
+            return withDetails('Service Unavailable - retry later');
+        case 504:
+            return withDetails('Gateway Timeout');
         default:
             return withDetails(`Error ${status}`);
     }
 }
 
 export const handleQueryError = (responseError: FetchBaseQueryError | SerializedError): string => {
+    // eslint-disable-next-line no-console
+    console.error('Query error:', responseError);
+
     if (!('status' in responseError)) {
         return String(responseError.message ?? responseError.name);
     }
 
     if (typeof responseError.status === 'number') {
-        let details: string;
+        let details: string | undefined;
 
         if (typeof responseError.data === 'object'
+            && responseError.data !== null
+            && 'message' in responseError.data
+            && typeof responseError.data.message === 'string'
+        ) {
+            details = responseError.data.message;
+        } else if (typeof responseError.data === 'object'
             && responseError.data !== null
             && 'error' in responseError.data
             && typeof responseError.data.error === 'string'
         ) {
             details = responseError.data.error;
+        } else if (typeof responseError.data === 'string') {
+            details = responseError.data;
         } else {
-            details = typeof responseError.data === 'string' ? responseError.data : JSON.stringify(responseError.data);
+            details = responseError.data ? JSON.stringify(responseError.data) : undefined;
         }
 
         return handleStatusCode(responseError.status, details);
